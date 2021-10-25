d33789c7221b18f853459df5b729550d3ad02fa7
tringuyen53 <nguyenduongtri0503@gmail.com>
2021-10-25 15:25:55 +0700

    test digest auth error

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,9 @@ async fn main() -> Result<(), reqwest::Error> {
     //     println!("Async task 3 started.");
     // });
 
-    // let client = reqwest::Client::builder()
+    let client = reqwest::Client::builder()
     // .timeout(time::Duration::from_secs(30))
-    // .build().unwrap();
+    .build().unwrap();
 
     // let client = reqwest::Client::new();
 
@@ -57,7 +57,7 @@ async fn main() -> Result<(), reqwest::Error> {
     // let cam_url = "http://10.50.29.64/axis-cgi/mjpg/video.cgi";
     // let cam_url = "http://vietnam:L3xRay123!@10.50.29.64/mjpg/1/video.mjpg"; //digest
     // let cam_url = "http://climatecam.gi.alaska.edu/mjpg/video.mjpg";
-    // let cam_url = "http://vietnam:L3xRay123!@10.50.31.178/mjpg/1/video.mjpg?resolution=640x480"; //digest
+    let cam_url = "http://vietnam:L3xRay123!@10.50.31.178/mjpg/1/video.mjpg"; //digest
     // let cam_url = "http://vietnam:L3xRay123!@10.50.29.117/mjpgstreamreq/1/image.jpg";
     // let cam_url = "http://vietnam:L3xRay123!@10.50.29.56/mjpgstreamreq/1/image.jpg";
     // let cam_url = "http://10.50.31.39/mjpg/1/video.mjpg?resolution=640x480";
@@ -79,7 +79,7 @@ async fn main() -> Result<(), reqwest::Error> {
     // let cam_url = "http://10.50.30.100/mjpgstreamreq/1/image.jpg";
     // let cam_url = "http://10.50.13.23/mjpgstreamreq/1/image.jpg";
     // let cam_url = "http://10.50.29.32/mjpgstreamreq/1/image.jpg";
-    let cam_url = "http://10.50.29.96/mjpgstreamreq/1/image.jpg";
+    // let cam_url = "http://10.50.29.96/mjpgstreamreq/1/image.jpg";
     // let cam_url = "http://10.50.30.197/mjpgstreamreq/1/image.jpg";
     // let cam_url = "http://10.50.29.77/mjpgstreamreq/1/image.jpg";
     // let cam_url = "http://10.50.30.118/mjpgstreamreq/1/image.jpg";
@@ -94,10 +94,10 @@ async fn main() -> Result<(), reqwest::Error> {
     // let cam_url = "http://10.50.31.169/mjpgstreamreq/1/image.jpg";
     let mut count: i32 = 1;
 
-    let client: Client = surf::Config::new()
-        .set_timeout(Some(std::time::Duration::from_secs(30)))
-        .try_into()
-        .unwrap();
+    // let client: Client = surf::Config::new()
+    //     .set_timeout(Some(std::time::Duration::from_secs(30)))
+    //     .try_into()
+    //     .unwrap();
 
     let mut answer = String::new();
     let basic_url = Url::parse(cam_url).unwrap();
@@ -123,47 +123,68 @@ async fn main() -> Result<(), reqwest::Error> {
     // println!("Port: {:?}", basic_url.port());
     println!("usr: {} - pwd: {}", username, password);
     // println!("[CAMERA] CAMERA STATUS {:?}", resp.status());
-    match client.get(digest_url.as_str()).send().await {
-        Ok(mut resp) => {
-            resp.take_body();
-            if let Some(value) = resp.header("www-authenticate") {
-                match value.to_string().split_once(' ').unwrap_or_default().0 {
-                    "Digest" => {
-                        println!("Digest Camera.");
-                        let mut prompt = digest_auth::parse(&value.to_string()).unwrap();
-                        let context = AuthContext::new(username, password, digest_url.path());
-                        answer = prompt.respond(&context).unwrap().to_header_string();
-                        println!("Answer: {:?}", answer);
-                    }
-                    _ => {}
-                }
+    // match client.get(digest_url.as_str()).send().await {
+    //     Ok(mut resp) => {
+    //         resp.take_body();
+    //         if let Some(value) = resp.header("www-authenticate") {
+    //             match value.to_string().split_once(' ').unwrap_or_default().0 {
+    //                 "Digest" => {
+    //                     println!("Digest Camera.");
+    //                     let mut prompt = digest_auth::parse(&value.to_string()).unwrap();
+    //                     let context = AuthContext::new(username, password, digest_url.path());
+    //                     answer = prompt.respond(&context).unwrap().to_header_string();
+    //                     println!("Answer: {:?}", answer);
+    //                 }
+    //                 _ => {}
+    //             }
+    //         }
+    //         let response = match answer.as_str() {
+    //             "" => client.get(basic_url.as_str()).send().await,
+    //             _ => {
+    //                 client
+    //                     .get(digest_url.as_str())
+    //                     .header("authorization", answer.clone())
+    //                     .send()
+    //                     .await
+    //             }
+    //         };
+    //         match response {
+    //             Ok(mut response) => {
+    //                 let status_code = response.status().to_string();
+    //                 println!("[CAMERA] CAMERA STATUS {:?}", status_code);
+    //                 response.take_body();
+    //             }
+    //             Err(e) => {
+    //                 println!("Response error: {:?}", e);
+    //             }
+    //         }
+    //     },
+    //     Err(e) => {
+    //         println!("Response first error: {:?}", e);
+    //     }
+    // }
+    let response = surf::get(digest_url.as_str()).await.unwrap();
+    let mut answer = String::new();
+    println!("response header: {:?}", response.header("www-authenticate").unwrap());
+
+    match response.header("www-authenticate") {
+        Some(value) => {
+            println!("Value: {:?}", value.as_str().split_once(' ').unwrap().0);
+            match value.as_str().split_once(' ').unwrap().0 {
+            "Digest" => {
+                println!("Digest");
+                let mut prompt = digest_auth::parse(&value.as_str()).unwrap();
+                let context = AuthContext::new(username, password, digest_url.as_str());
+                answer = prompt.respond(&context).unwrap().to_header_string();
             }
-            let response = match answer.as_str() {
-                "" => client.get(basic_url.as_str()).send().await,
-                _ => {
-                    client
-                        .get(digest_url.as_str())
-                        .header("authorization", answer.clone())
-                        .send()
-                        .await
-                }
-            };
-            match response {
-                Ok(mut response) => {
-                    let status_code = response.status().to_string();
-                    println!("[CAMERA] CAMERA STATUS {:?}", status_code);
-                    response.take_body();
-                }
-                Err(e) => {
-                    println!("Response error: {:?}", e);
-                }
+            _ => {
+                println!("Basic");
             }
-        },
-        Err(e) => {
-            println!("Response first error: {:?}", e);
-        }
+        }},
+        None => {answer = "".to_owned()}
     }
-    loop {}
+    println!("Answer: {:?}", answer);   
+    // loop {}
     
     // if let Some(value) = header.get("www-authenticate") {
     //     if let Ok(value) = value.to_str() {
@@ -183,129 +204,129 @@ async fn main() -> Result<(), reqwest::Error> {
     // drop(stream);
 
 
-    // let mut retry_count = 0;
-    // 'main: while retry_count < 6 {
-    //     // smol::Timer::after(std::time::Duration::from_secs(2u64.pow(retry_count)));
-    //     let response = match answer.as_str() {
-    //         "" => {
-    //             println!("Basic");
-    //             client
-    //                 .get(cam_url)
-    //                 // .timeout(time::Duration::from_secs(60))
-    //                 // .basic_auth(username, Some(password))
-    //                 // .header(reqwest::header::AUTHORIZATION, answer.clone())
-    //                 .send()
-    //                 .await
-    //         }
-    //         _ => {
-    //             println!("Digest");
-    //             client
-    //                 .get(digest_url.as_str())
-    //                 // .header(CONNECTION, "close")
-    //                 // .basic_auth("vietnam", Some("L3xRay123!"))
-    //                 .header(reqwest::header::AUTHORIZATION, answer.clone())
-    //                 .send()
-    //                 .await
-    //         }
-    //     };
-    //     if response.is_err() {
-    //         smol::Timer::after(std::time::Duration::from_secs(2u64.pow(retry_count))).await;
-    //         println!("Response error after {:?} retry: {:?}", retry_count, response.err());
+    let mut retry_count = 0;
+    'main: while retry_count < 6 {
+        // smol::Timer::after(std::time::Duration::from_secs(2u64.pow(retry_count)));
+        let response = match answer.as_str() {
+            "" => {
+                println!("Basic");
+                client
+                    .get(basic_url.as_str())
+                    // .timeout(time::Duration::from_secs(60))
+                    // .basic_auth(username, Some(password))
+                    // .header(reqwest::header::AUTHORIZATION, answer.clone())
+                    .send()
+                    .await
+            }
+            _ => {
+                println!("Digest {:?}: {:?}",digest_url.as_str(), answer.clone());
+                client
+                    .get(digest_url.as_str())
+                    // .header(CONNECTION, "close")
+                    // .basic_auth("vietnam", Some("L3xRay123!"))
+                    .header(reqwest::header::AUTHORIZATION, answer.clone())
+                    .send()
+                    .await
+            }
+        };
+        if response.is_err() {
+            smol::Timer::after(std::time::Duration::from_secs(2u64.pow(retry_count))).await;
+            println!("Response error after {:?} retry: {:?}", retry_count, response.err());
 
-    //         retry_count += 1;
-    //         continue;
-    //         // break;
-    //     } else {
-    //         retry_count = 0;
-    //     }
-    //     let response = response.unwrap();
-    //     let status_code = response.status().as_u16();
-    //     println!("[CAMERA] CAMERA STATUS {:?}", status_code);
-    //     if status_code == 401 {
-    //         println!("Unauthorized");
-    //         // *is_frame_getting.lock().unwrap() = false;
-    //         // retry_count += 1;
-    //         break 'main;
-    //     }
+            retry_count += 1;
+            continue;
+            // break;
+        } else {
+            retry_count = 0;
+        }
+        let response = response.unwrap();
+        let status_code = response.status().as_u16();
+        println!("[CAMERA] CAMERA STATUS {:?}", response);
+        if status_code == 401 {
+            println!("Unauthorized");
+            // *is_frame_getting.lock().unwrap() = false;
+            // retry_count += 1;
+            break 'main;
+        }
 
-    //     let mut stream = response.bytes_stream();
-    //     while let Some(chunk) = stream.next().await {
-    //         if chunk.is_err() {
-    //             continue;
-    //         }
-    //         let chunk_fake = chunk.unwrap().clone();
-    //         store_buffer.extend_from_slice(&chunk_fake);
+        let mut stream = response.bytes_stream();
+        while let Some(chunk) = stream.next().await {
+            if chunk.is_err() {
+                continue;
+            }
+            let chunk_fake = chunk.unwrap().clone();
+            store_buffer.extend_from_slice(&chunk_fake);
 
-    //         let mut i = 0;
-    //         let length = store_buffer.len();
-    //         // println!("chunk length: {}", chunk_fake.len());
-    //         // println!(
-    //         //     "Before traverse through store buffer: {}",
-    //         //     store_buffer.len()
-    //         // );
-    //         // println!("START LOOP");
-    //         while i < length {
-    //             if i == length - 1 {
-    //                 frame_buffer.push(store_buffer[i]);
-    //                 store_buffer.clear();
-    //                 break;
-    //             }
+            let mut i = 0;
+            let length = store_buffer.len();
+            // println!("chunk length: {}", chunk_fake.len());
+            // println!(
+            //     "Before traverse through store buffer: {}",
+            //     store_buffer.len()
+            // );
+            // println!("START LOOP");
+            while i < length {
+                if i == length - 1 {
+                    frame_buffer.push(store_buffer[i]);
+                    store_buffer.clear();
+                    break;
+                }
 
-    //             if store_buffer[i] == NONE && store_buffer[i + 1] == EOI && is_begin_frame == true {
-    //                 frame_buffer.push(store_buffer[i]);
-    //                 frame_buffer.push(EOI);
-    //                 println!(
-    //                     "[FRAME] END OF FRAME (from {}): {}",
-    //                     cam_url,
-    //                     frame_buffer.len()
-    //                 );
+                if store_buffer[i] == NONE && store_buffer[i + 1] == EOI && is_begin_frame == true {
+                    frame_buffer.push(store_buffer[i]);
+                    frame_buffer.push(EOI);
+                    println!(
+                        "[FRAME] END OF FRAME (from {}): {}",
+                        cam_url,
+                        frame_buffer.len()
+                    );
 
-    //                 //SAVING IMAGE
-    //                 // let img_result =
-    //                 //     image::load_from_memory_with_format(&frame_buffer, ImageFormat::Jpeg);
-    //                 // let img = match img_result {
-    //                 //     Ok(image) => image,
-    //                 //     Err(_) => return Err(()),
-    //                 // };
-    //                 // img.save(format!("img-{}.jpg", count)).unwrap();
-    //                 // let img16 = img.into_rgb8();
-    //                 // let data = img16.into_raw() as Vec<u8>;
-    //                 // println!("Image length: {}", data.len());
-    //                 count += 1;
+                    //SAVING IMAGE
+                    // let img_result =
+                    //     image::load_from_memory_with_format(&frame_buffer, ImageFormat::Jpeg);
+                    // let img = match img_result {
+                    //     Ok(image) => image,
+                    //     Err(_) => return Err(()),
+                    // };
+                    // img.save(format!("img-{}.jpg", count)).unwrap();
+                    // let img16 = img.into_rgb8();
+                    // let data = img16.into_raw() as Vec<u8>;
+                    // println!("Image length: {}", data.len());
+                    count += 1;
 
-    //                 let _drain: Vec<_> = store_buffer.drain(..=i + 1).collect();
-    //                 is_begin_frame = false;
-    //                 frame_buffer.clear();
+                    let _drain: Vec<_> = store_buffer.drain(..=i + 1).collect();
+                    is_begin_frame = false;
+                    frame_buffer.clear();
 
-    //                 println!("store buffer: {}", store_buffer.len());
-    //                 println!("frame buffer: {}", frame_buffer.len());
-    //                 break;
-    //             }
+                    println!("store buffer: {}", store_buffer.len());
+                    println!("frame buffer: {}", frame_buffer.len());
+                    break;
+                }
 
-    //             if is_begin_frame == true {
-    //                 frame_buffer.push(store_buffer[i]);
-    //             }
+                if is_begin_frame == true {
+                    frame_buffer.push(store_buffer[i]);
+                }
 
-    //             if store_buffer[i] == NONE && store_buffer[i + 1] == SOI {
-    //                 println!("[FRAME] START OF FRAME (from {})", cam_url);
+                if store_buffer[i] == NONE && store_buffer[i + 1] == SOI {
+                    println!("[FRAME] START OF FRAME (from {})", cam_url);
 
-    //                 is_begin_frame = true;
-    //                 frame_buffer.push(store_buffer[i]);
-    //                 frame_buffer.push(SOI);
-    //                 i += 1;
-    //             }
-    //             i += 1;
-    //         }
-    //         // println!("END LOOP");
-    //         if store_buffer.len() == 18 {
-    //             println!("Fuk im out!");
-    //             store_buffer.clear();
-    //             break;
-    //         }
-    //     }
-    //     println!("End of request");
-    // }
-    // Ok(())
+                    is_begin_frame = true;
+                    frame_buffer.push(store_buffer[i]);
+                    frame_buffer.push(SOI);
+                    i += 1;
+                }
+                i += 1;
+            }
+            // println!("END LOOP");
+            if store_buffer.len() == 18 {
+                println!("Fuk im out!");
+                store_buffer.clear();
+                break;
+            }
+        }
+        println!("End of request");
+    }
+    Ok(())
     // tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
     // println!("End of sleep");
     // return Ok(());
